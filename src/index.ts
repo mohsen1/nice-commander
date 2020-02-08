dd574d0a1f8f5ab75edd8ba8f370dd7e48e8373b
Mohsen Azimi <mohsen.azimi@airbnb.com>
2020-02-07 19:19:24 -0500

    Fix main field in package.json

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import NiceCommander from "./api/core";
 import { TaskDefinition } from "./api/core/TaskDefinition";
 
 export default NiceCommander;
+module.exports = NiceCommander;
 export { TaskDefinition };
