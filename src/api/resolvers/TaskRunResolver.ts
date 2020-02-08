63fb804be3f7c57af7eae3a8673438e81ad0da48
Mohsen Azimi <mohsen.azimi@airbnb.com>
2020-02-07 20:56:11 -0500

    More fixes

--- src/api/resolvers/TaskRunResolver.ts
+++ src/api/resolvers/TaskRunResolver.ts
@@ -70,6 +70,16 @@ export function getTasksRunResolver(
       // start the task
       await niceCommander.startTask(taskRun);
 
+      // Assign logs
+      try {
+        const logs = await niceCommander.getLogsFromS3(taskRun.logsPath);
+        if (logs) {
+          taskRun.logs = logs;
+        }
+      } catch {
+        taskRun.logs = "logs are not available yet";
+      }
+
       // Save to DB
       await this.repository.save(taskRun);
 
