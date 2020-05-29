7a713339eb0516a10ee17678288ebd1a94317c08
Mohsen Azimi <mohsen.azimi@airbnb.com>
2020-05-29 13:55:22 -0400

    Use DI to allow declaration emit

--- src/api/resolvers/TasksResolver.ts
+++ src/api/resolvers/TasksResolver.ts
@@ -1,65 +1,64 @@
 import { Resolver, Query, Arg, Int } from "type-graphql";
 import { Connection } from "typeorm";
+import { Service, Inject } from "typedi";
 
 import { Task } from "../models/Task";
 import { assertNumberArgumentIsInRange } from "./util";
 
-export function getTasksResolver(connection: Connection) {
-  @Resolver(Task)
-  class TasksResolver {
-    private get repository() {
-      const taskRepository = connection.getRepository(Task);
-      return taskRepository;
-    }
-
-    @Query((returns) => [Task], { description: "Get a list of tasks" })
-    async tasks(
-      @Arg("take", (type) => Int, {
-        defaultValue: 10,
-        nullable: true,
-        description: "How many to take",
-      })
-      take: number,
-      @Arg("skip", (type) => Int, {
-        defaultValue: 0,
-        nullable: true,
-        description: "How many to skip",
-      })
-      skip: number
-    ) {
-      assertNumberArgumentIsInRange("take", take, 1, 500);
-      assertNumberArgumentIsInRange("skip", skip, 0, Infinity);
-
-      return this.repository.find({
-        take,
-        skip,
-        relations: ["runs"],
-      });
-    }
-
-    @Query((returns) => Task, {
-      description: "Get a single task",
-      nullable: true,
-    })
-    async task(@Arg("name", { description: "Task unique name" }) name: string) {
-      const task = await this.repository.findOne({
-        where: { name },
-        relations: ["runs"],
-      });
-
-      // Avoid joint if not necessary
-      if (task && !task.runs.length) {
-        return task;
-      }
-
-      return this.repository
-        .createQueryBuilder("task")
-        .where({ name })
-        .innerJoinAndSelect("task.runs", "runs")
-        .orderBy("runs.startTime", "DESC")
-        .getOne();
-    }
+@Service()
+@Resolver(Task)
+export default class TasksResolver {
+  constructor(@Inject("connection") private readonly connection: Connection) {}
+  private get repository() {
+    const taskRepository = this.connection.getRepository(Task);
+    return taskRepository;
   }
 
-  return TasksResolver;
+  @Query((returns) => [Task], { description: "Get a list of tasks" })
+  async tasks(
+    @Arg("take", (type) => Int, {
+      defaultValue: 10,
+      nullable: true,
+      description: "How many to take",
+    })
+    take: number,
+    @Arg("skip", (type) => Int, {
+      defaultValue: 0,
+      nullable: true,
+      description: "How many to skip",
+    })
+    skip: number
+  ) {
+    assertNumberArgumentIsInRange("take", take, 1, 500);
+    assertNumberArgumentIsInRange("skip", skip, 0, Infinity);
+
+    return this.repository.find({
+      take,
+      skip,
+      relations: ["runs"],
+    });
+  }
+
+  @Query((returns) => Task, {
+    description: "Get a single task",
+    nullable: true,
+  })
+  async task(@Arg("name", { description: "Task unique name" }) name: string) {
+    const task = await this.repository.findOne({
+      where: { name },
+      relations: ["runs"],
+    });
+
+    // Avoid joint if not necessary
+    if (task && !task.runs.length) {
+      return task;
+    }
+
+    return this.repository
+      .createQueryBuilder("task")
+      .where({ name })
+      .innerJoinAndSelect("task.runs", "runs")
+      .orderBy("runs.startTime", "DESC")
+      .getOne();
+  }
 }
